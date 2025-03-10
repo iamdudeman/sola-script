ce0a501e2a231ab230e7ec6ef2876dec6cbc0d44
iamdudeman <timothy.solum@gmail.com>
2025-03-10 17:20:22 -0500

    added identifier and keyword support

--- src/main/java/technology/jlox/Scanner.java
+++ src/main/java/technology/jlox/Scanner.java
@@ -1,14 +1,36 @@
 package technology.jlox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Scanner {
+  private static final Map<String, TokenType> KEYWORDS = new HashMap<>();
   private final String source;
   private final List<Token> tokens = new ArrayList<>();
   private int start = 0;
   private int current = 0;
   private int line = 1;
+
+  static {
+    KEYWORDS.put("and", TokenType.AND);
+    KEYWORDS.put("class", TokenType.CLASS);
+    KEYWORDS.put("else", TokenType.ELSE);
+    KEYWORDS.put("false", TokenType.FALSE);
+    KEYWORDS.put("for", TokenType.FOR);
+    KEYWORDS.put("fun", TokenType.FUN);
+    KEYWORDS.put("if", TokenType.IF);
+    KEYWORDS.put("nil", TokenType.NIL);
+    KEYWORDS.put("or", TokenType.OR);
+    KEYWORDS.put("print", TokenType.PRINT);
+    KEYWORDS.put("return", TokenType.RETURN);
+    KEYWORDS.put("super", TokenType.SUPER);
+    KEYWORDS.put("this", TokenType.THIS);
+    KEYWORDS.put("true", TokenType.TRUE);
+    KEYWORDS.put("var", TokenType.VAR);
+    KEYWORDS.put("while", TokenType.WHILE);
+  }
 
   public Scanner(String source) {
     this.source = source;
@@ -71,11 +93,24 @@ public class Scanner {
       default:
         if (isDigit(c)) {
           number();
+        } else if (isAlpha(c)) {
+          identifier();
         } else {
           Lox.error(line, "Unexpected character '" + c + "'");
         }
         break;
     }
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) {
+      advance();
+    }
+
+    String text = source.substring(start, current);
+    TokenType type = KEYWORDS.getOrDefault(text, TokenType.IDENTIFIER);
+
+    addToken(type);
   }
 
   private void number() {
@@ -146,6 +181,14 @@ public class Scanner {
 
   private boolean isDigit(char c) {
     return c >= '0' && c <= '9';
+  }
+
+  private boolean isAlpha(char c) {
+    return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
   }
 
   private boolean isAtEnd() {
