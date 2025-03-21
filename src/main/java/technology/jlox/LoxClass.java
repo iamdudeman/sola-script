26692f9cd587614ae56bd3c86da20ddfe103804b
iamdudeman <timothy.solum@gmail.com>
2025-03-21 09:57:30 -0500

    instances of a class can now be created

--- src/main/java/technology/jlox/LoxClass.java
+++ src/main/java/technology/jlox/LoxClass.java
@@ -1,10 +1,24 @@
 package technology.jlox;
 
-class LoxClass {
+import java.util.List;
+
+class LoxClass implements LoxCallable {
   final String name;
 
   public LoxClass(String name) {
     this.name = name;
+  }
+
+  @Override
+  public int arity() {
+    return 0;
+  }
+
+  @Override
+  public Object call(Interpreter interpreter, List<Object> arguments) {
+    LoxInstance instance = new LoxInstance(this);
+
+    return instance;
   }
 
   @Override
---
+++ src/main/java/technology/jlox/LoxInstance.java
@@ -0,0 +1,14 @@
+package technology.jlox;
+
+class LoxInstance {
+  private LoxClass klass;
+
+  public LoxInstance(LoxClass klass) {
+    this.klass = klass;
+  }
+
+  @Override
+  public String toString() {
+    return klass.name + " instance";
+  }
+}
