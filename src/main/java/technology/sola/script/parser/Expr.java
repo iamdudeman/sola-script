e1e32417130a582298f7e8edea8289670bf04882
iamdudeman <timothy.solum@gmail.com>
2025-03-24 17:01:12 -0500

    initial structure of Expr and Stmt

--- src/main/java/technology/sola/script/parser/Expr.java
+++ src/main/java/technology/sola/script/parser/Expr.java
@@ -1,4 +1,18 @@
 package technology.sola.script.parser;
 
-public abstract class Expr {
+import technology.sola.script.tokenizer.Token;
+
+public interface Expr {
+  <R> R visit(Visitor<R> visitor);
+
+  interface Visitor<R> {
+    R visitAssignExpr(Assign expr);
+  }
+
+  record Assign(Token name, Expr value) implements Expr {
+    @Override
+    public <R> R visit(Visitor<R> visitor) {
+      return visitor.visitAssignExpr(this);
+    }
+  }
 }
--- src/main/java/technology/sola/script/parser/Stmt.java
+++ src/main/java/technology/sola/script/parser/Stmt.java
@@ -1,4 +1,18 @@
 package technology.sola.script.parser;
 
-public abstract class Stmt {
+import java.util.List;
+
+public interface Stmt {
+  <R> R accept(Visitor<R> visitor);
+
+  interface Visitor<R> {
+    R visitBlockStmt(Block stmt);
+  }
+
+  record Block(List<Stmt> statements) implements Stmt {
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBlockStmt(this);
+    }
+  }
 }
