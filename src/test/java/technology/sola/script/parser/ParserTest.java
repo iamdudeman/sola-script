258ffab6e8e68bc4d638f11fd4a339b016522391
iamdudeman <timothy.solum@gmail.com>
2025-03-28 10:56:22 -0500

    cleaned up ParserTest more

--- src/test/java/technology/sola/script/parser/ParserTest.java
+++ src/test/java/technology/sola/script/parser/ParserTest.java
@@ -406,20 +406,4 @@ class ParserTest {
       }
     }
   }
-
-  private TestResult visualizeScriptParsing(String source) {
-    var tokenizer = new Tokenizer(source);
-    var parser = new Parser(tokenizer.tokenize().tokens());
-    var parserResultPrinter = new ParserResultPrinter();
-    var parserResult = parser.parse();
-    var parsedScript = parserResultPrinter.print(parserResult);
-
-    return new TestResult(
-      parsedScript,
-      parserResult.errors()
-    );
-  }
-
-  private record TestResult(String parsedScript, List<ScriptError> errors) {
-  }
 }
