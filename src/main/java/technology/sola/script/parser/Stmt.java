86a7b84bac001e840ae11f0b009df8d476d81755
iamdudeman <timothy.solum@gmail.com>
2025-04-02 18:04:18 -0500

    added missing JavaDocs to Stmt

--- src/main/java/technology/sola/script/parser/Stmt.java
+++ src/main/java/technology/sola/script/parser/Stmt.java
@@ -23,6 +23,12 @@ public interface Stmt {
    * @param <R> the return type of the executed statement
    */
   interface Visitor<R> {
+    /**
+     * Executes a {@link Var} statement and returns the value.
+     *
+     * @param stmt the {@link Var} statement
+     * @return the evaluated value
+     */
     R var(Var stmt);
 
     /**
@@ -33,11 +39,29 @@ public interface Stmt {
      */
     R expression(Expression stmt);
 
+    /**
+     * Executes an {@link If} statement and returns the value.
+     *
+     * @param stmt the {@link If} statement
+     * @return the evaluated value
+     */
     R ifVisit(If stmt);
 
+    /**
+     * Executes a {@link Block} and returns the value.
+     *
+     * @param stmt the {@link Block} statement
+     * @return the evaluated value
+     */
     R block(Block stmt);
   }
 
+  /**
+   * Var statement declares a variable in the current scope with an optional initializer.
+   *
+   * @param name        the {@link Token} name of the variable
+   * @param initializer the expression to initialize the variable with
+   */
   record Var(Token name, Expr initializer) implements Stmt {
     @Override
     public <R> R accept(Visitor<R> visitor) {
@@ -45,6 +69,11 @@ public interface Stmt {
     }
   }
 
+  /**
+   * Expression statement evaluates an expression.
+   *
+   * @param expr the expression to evaluate
+   */
   record Expression(Expr expr) implements Stmt {
     @Override
     public <R> R accept(Visitor<R> visitor) {
@@ -52,6 +81,15 @@ public interface Stmt {
     }
   }
 
+  /**
+   * If statement is a conditional branching statement. If the condition evaluates
+   * to {@link technology.sola.script.interpreter.ValueUtils#isTruthy(Object)} then the thenBranch {@link Stmt} will be
+   * executed. If the condition evaluates to not truthy then the optional else branch will execute if it is provided.
+   *
+   * @param condition  the condition to branch from
+   * @param thenBranch the truthy branch to execute
+   * @param elseBranch the optional not truthy branch to execute
+   */
   record If(Expr condition, Stmt thenBranch, Stmt elseBranch) implements Stmt {
     @Override
     public <R> R accept(Visitor<R> visitor) {
@@ -59,6 +97,11 @@ public interface Stmt {
     }
   }
 
+  /**
+   * Block contains a list of {@link Stmt}s that are executed in their own scope.
+   *
+   * @param statements the list of {@link Stmt}s to execute
+   */
   record Block(List<Stmt> statements) implements Stmt {
     @Override
     public <R> R accept(Visitor<R> visitor) {
