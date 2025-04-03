e44cbec0865f5db1bf3b91e6ecf8f4ccf232324a
iamdudeman <timothy.solum@gmail.com>
2025-04-03 15:10:29 -0500

    added tests and docs for SolaScriptFunction

--- src/main/java/technology/sola/script/runtime/SolaScriptFunction.java
+++ src/main/java/technology/sola/script/runtime/SolaScriptFunction.java
@@ -5,12 +5,23 @@ import technology.sola.script.parser.Stmt;
 import java.util.List;
 import java.util.function.Consumer;
 
+/**
+ * SolaScriptFunction is a runtime definition of a function declaration that is callable (implements
+ * {@link SolaScriptCallable} interface).
+ */
 public class SolaScriptFunction implements SolaScriptCallable {
   private final Consumer<Stmt> executeStatement;
   private final Stmt.Function declaration;
   private final ScriptRuntime scriptRuntime;
   private final Environment closure;
 
+  /**
+   * Creates an instance of the runtime function declaration.
+   *
+   * @param executeStatement the consumer that handles execution at runtime
+   * @param scriptRuntime    the {@link ScriptRuntime} state
+   * @param declaration      the {@link Stmt.Function} declaration
+   */
   public SolaScriptFunction(Consumer<Stmt> executeStatement, ScriptRuntime scriptRuntime, Stmt.Function declaration) {
     this.executeStatement = executeStatement;
     this.scriptRuntime = scriptRuntime;
