d4c11eb1e2e482cb04dbeb33c70fcafc0a685a18
iamdudeman <timothy.solum@gmail.com>
2025-04-04 13:31:08 -0500

    added docs for SolaSCriptMap

--- src/main/java/technology/sola/script/runtime/SolaScriptMap.java
+++ src/main/java/technology/sola/script/runtime/SolaScriptMap.java
@@ -5,18 +5,39 @@ import technology.sola.script.tokenizer.Token;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * SolaScriptMap is the runtime representation of the sola-script map type.
+ */
 public class SolaScriptMap {
-  private final Map<String, Object> fields = new HashMap<>();
+  private final Map<String, Object> properties = new HashMap<>();
 
+  /**
+   * Gets a property from the map.
+   *
+   * @param name the {@link Token} name of the property
+   * @return the value associated with the property or null
+   */
   public Object get(Token name) {
-    return fields.get(name.lexeme());
+    return properties.get(name.lexeme());
   }
 
+  /**
+   * Sets a property on the map.
+   *
+   * @param name  the {@link Token} name of the property
+   * @param value the value of the property
+   */
   public void set(Token name, Object value) {
     set(name.lexeme(), value);
   }
 
+  /**
+   * Sets a property on the map.
+   *
+   * @param name  the string name of the property
+   * @param value the value of the property
+   */
   public void set(String name, Object value) {
-    fields.put(name, value);
+    properties.put(name, value);
   }
 }
