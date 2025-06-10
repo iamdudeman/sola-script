d03493048e9b1184ffdb1ba387faf5c9e2701af5
iamdudeman <timothy.solum@gmail.com>
2025-06-09 21:33:31 -0500

    introduced SolaScript class that creates runtimes for executing scripts. added automated tests for all sample test programs use cases

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module technology.sola.script {
 
   exports technology.sola.script;
   exports technology.sola.script.error;
+  exports technology.sola.script.library;
   exports technology.sola.script.parser;
   exports technology.sola.script.tokenizer;
 }
