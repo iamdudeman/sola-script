bfa72c5c4e8e66442c47d366ea6b645c9666e2cd
iamdudeman <timothy.solum@gmail.com>
2025-06-13 17:26:55 -0500

    started adding missing javadocs

--- src/main/java/technology/sola/script/tokenizer/TokenType.java
+++ src/main/java/technology/sola/script/tokenizer/TokenType.java
@@ -5,13 +5,108 @@ package technology.sola.script.tokenizer;
  */
 public enum TokenType {
   // single character tokens
-  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-  COMMA, DOT, MINUS, PLUS, COLON, SEMICOLON, SLASH, STAR,
+  /**
+   * Single character token - "("
+   */
+  LEFT_PAREN,
+  /**
+   * Single character token - ")"
+   */
+  RIGHT_PAREN,
+  /**
+   * Single character token - "{"
+   */
+  LEFT_BRACE,
+  /**
+   * Single character token - "}"
+   */
+  RIGHT_BRACE,
+  /**
+   * Single character token - ","
+   */
+  COMMA,
+  /**
+   * Single character token - "."
+   */
+  DOT,
+  /**
+   * Single character token - "-"
+   */
+  MINUS,
+  /**
+   * Single character token - "+"
+   */
+  PLUS,
+  /**
+   * Single character token - ":"
+   */
+  COLON,
+  /**
+   * Single character token - ";"
+   */
+  SEMICOLON,
+  /**
+   * Single character token - "/"
+   */
+  SLASH,
+  /**
+   * Single character token - "*"
+   */
+  STAR,
 
   // One or two character tokens
-  BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL,
-  GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
-  AMP_AMP, BAR_BAR, QUESTION, QUESTION_QUESTION, QUESTION_DOT,
+  /**
+   * Single or double character token - "!"
+   */
+  BANG,
+  /**
+   * Single or double character token - "!="
+   */
+  BANG_EQUAL,
+  /**
+   * Single or double character token - "="
+   */
+  EQUAL,
+  /**
+   * Single or double character token - "=="
+   */
+  EQUAL_EQUAL,
+  /**
+   * Single or double character token - ">"
+   */
+  GREATER,
+  /**
+   * Single or double character token - ">="
+   */
+  GREATER_EQUAL,
+  /**
+   * Single or double character token - "<"
+   */
+  LESS,
+  /**
+   * Single or double character token - "<="
+   */
+  LESS_EQUAL,
+  /**
+   * Single or double character token - "&&"
+   */
+  AMP_AMP,
+  /**
+   * Single or double character token - "||"
+   */
+  BAR_BAR,
+  /**
+   * Single or double character token - "?"
+   */
+  QUESTION,
+  /**
+   * Single or double character token - "??"
+   */
+  QUESTION_QUESTION,
+  /**
+   * Single or double character token - "?."
+   */
+  QUESTION_DOT,
 
   // Literals
   IDENTIFIER, STRING, NUMBER,
